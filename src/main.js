c8b54e120bc84fda5f07f54f02ac42609d87e5ec
robi <robert.szaloki@euedge.com>
2018-03-09 15:34:08 +0100

    store cleanup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import loadScript from 'load-script'
 
 Vue.config.productionTip = false
 
 router.beforeEach((to, from, next) => {
-  console.log(to.matched)
   if (!store.state.gapiLoaded && to.matched.some(record => record.meta.chkinit)) {
     next({name: 'spinner', replace: true})
     return
@@ -30,3 +30,20 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+loadScript('https://apis.google.com/js/api.js', () => {
+  window.gapi.load('client:auth2', function () {
+    window.gapi.client.init({
+      apiKey: 'AIzaSyAjK1mi8amRdHYTQbeZIdGUlCH7mahevxg',
+      clientId: '960214299334-4nc7ad24u3cenam0j1bo4d3t5cbh5akg.apps.googleusercontent.com',
+      scope: 'profile https://www.googleapis.com/auth/drive.file'
+    }).then(function () {
+      // Listen for sign-in state changes.
+      window.gapi.auth2.getAuthInstance().isSignedIn.listen(status => store.commit('updateSignInStatus', status))
+
+      const status = window.gapi.auth2.getAuthInstance().isSignedIn.get()
+      store.commit('updateSignInStatus', status)
+      store.commit('gapiLoaded')
+    }).catch(e => console.error(e))
+  })
+})
--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import loadScript from 'load-script'
 
 Vue.use(Vuex)
 
@@ -39,23 +38,6 @@ const store = new Vuex.Store({
       }
     }
   }
-})
-
-loadScript('https://apis.google.com/js/api.js', () => {
-  window.gapi.load('client:auth2', function () {
-    window.gapi.client.init({
-      apiKey: 'AIzaSyAjK1mi8amRdHYTQbeZIdGUlCH7mahevxg',
-      clientId: '960214299334-4nc7ad24u3cenam0j1bo4d3t5cbh5akg.apps.googleusercontent.com',
-      scope: 'profile https://www.googleapis.com/auth/drive.file'
-    }).then(function () {
-      // Listen for sign-in state changes.
-      window.gapi.auth2.getAuthInstance().isSignedIn.listen(status => store.commit('updateSignInStatus', status))
-
-      const status = window.gapi.auth2.getAuthInstance().isSignedIn.get()
-      store.commit('updateSignInStatus', status)
-      store.commit('gapiLoaded')
-    }).catch(e => console.error(e))
-  })
 })
 
 export default store
